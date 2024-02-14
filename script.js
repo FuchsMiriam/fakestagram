c576e0398cbcc6f968ce53e6d460e12c5612898d
Miriam Fuchs <fuchsmiriam@yahoo.com>
2024-02-14 12:43:41 +0100

    changed Like-Section

--- script.js
+++ script.js
@@ -4,42 +4,54 @@ let posts = [
         'image': './img/schottland.jpg',
         'location': 'Edinburgh, Schottland',
         'description': '🏴󠁧󠁢󠁳󠁣󠁴󠁿✨ Tauchen Sie ein und entdecken Sie die zehn schönsten Orte in Schottland! Von majestätischen Burgen bis zu atemberaubenden Landschaften - diese Reise wird Ihr Herz erobern. 🏰🏞️ #Schottland #Reiseziele #Entdeckung #Abenteuer 🌍',
-        'comments': []
+        'comments': [],
+        'likes': 364,
+        'liked': false,
     },
     {
         'author': 'Miriam Fuchs',
         'image': './img/cooking.jpg',
         'location': 'Taunusstein, Deutschland',
         'description': '🥟👩‍🍳 Lust auf köstliche Teigtaschen, ohne viel Aufwand? 🌟 Entdecken Sie, wie einfach es ist, diese Leckereien zuzubereiten und Ihre Geschmacksknospen zu verwöhnen! 🍽️💫 #Kochtipps #Teigtaschen #Genusspur 🌮👌',
-        'comments': []
+        'comments': [],
+        'likes': 89,
+        'liked': false,
     },
     {
         'author': 'Tarou Yamada',
         'image': './img/zeitung.jpg',
         'location': 'Frankfurt, Deutschland',
         'description': '📰📈 Studie zeigt: Wieder mehr Menschen greifen zur Tageszeitung! 🗞️💼 Erfahren Sie, warum dieses traditionsreiche Medium wieder an Beliebtheit gewinnt und welche Vorteile es bietet. 📊🤓 #Tageszeitung #Medien #Informationsquelle 🗞️🔍',
-        'comments': []
+        'comments': [],
+        'likes': 152,
+        'liked': false,
     },
     {
         'author': 'Vogelschutzverein Musterstadt',
         'image': './img/birdFeeder.jpg',
         'location': 'Musterstadt, Deutschland',
         'description': '🐦❄️ Unterstützen Sie Vögel durch die Winterzeit mit schmackhaften Snacks! Entdecken Sie einfache Möglichkeiten, wie Sie unseren gefiederten Freunden helfen können, diese kalte Jahreszeit zu überstehen. 🥜🐦 #Vogelfütterung #Winterhilfe #Naturliebe 🌿🕊️',
-        'comments': []
+        'comments': [],
+        'likes': 44,
+        'liked': false,
     },
     {
         'author': 'Kleinkunstverein Wiesbaden',
         'image': './img/saxophon.jpg',
         'location': 'Wiesbaden, Deutschland',
         'description': '🎷🎶 Große Neuigkeiten für Musikliebhaber: Candy Dulfer kommt im April für ein Konzert nach Deutschland! 🎵🎉 Seien Sie dabei, wenn diese Jazz-Ikone die Bühne rockt und für unvergessliche Momente sorgt. 🎤🎶 #CandyDulfer #Konzert #LiveMusik 🎷',
-        'comments': []
+        'comments': [],
+        'likes': 56,
+        'liked': false,
     },
     {
         'author': 'Smart & Fit',
         'image': './img/sport.jpg',
         'location': 'New York, USA',
         'description': '💪🏼🧠 Besiegen Sie den inneren Schweinehund und erreichen Sie Ihre Ziele! 🏋🏽‍♀️🎯 Erfahren Sie, wie Sie Ihre Motivation steigern und Hindernisse überwinden, um erfolgreich zu sein. 🌟 #Motivation #Zielsetzung #Erfolg 💼🚀',
-        'comments': []
+        'comments': [],
+        'likes': 783,
+        'liked': false,
     }
 ];
 
@@ -61,10 +73,12 @@ function show() {
         </div>
         <img class="postImages" src="${post['image']}">
         <div class="likeSection">
-          <img class="heart" src="${liked ? './img/liked.png' : './img/like.png'}" alt="${liked ? 'Liked' : 'Like'}" onclick="imageChange(${i})" id="likeImage${i}">
-          <div class="likeText"><span>Gefällt&nbsp;</span>
-          <span class="likeCounter" id="likeCounter${i}">${liked ? '1' : '45'}</span>
-          <span>&nbsp;Mal</span></div>
+        <div class="heart" onclick="imageChange(${i})" id="likeImage${i}">${post.liked ? '❤️' : '🤍'}</div>
+        <div class="likeText">
+            <span>Gefällt&nbsp;</span>
+            <span class="likeCounter" id="likeCounter${i}">${post.likes > 0 ? post.likes : ''}</span>
+            <span>&nbsp;Mal</span>
+        </div>
         </div>
         <div class="postDescription">${post['description']}</div>
         <div id="commentContainer${i}"></div>
@@ -82,25 +96,20 @@ function show() {
 
 
 function imageChange(i) {
+    let post = posts[i];
     let imageElement = document.getElementById(`likeImage${i}`);
     let counterElement = document.getElementById(`likeCounter${i}`);
-    let count = parseInt(counterElement.textContent) || 45;
-
-    if (imageElement.src.endsWith('like.png')) {
-        imageElement.src = './img/liked.png';
-        imageElement.alt = 'Liked';
-        count += 1;
-        counterElement.textContent = count;
-        localStorage.setItem(`liked${i}`, 'true');
+    if (post.liked) {
+        post.liked = false;
+        post.likes -= 1;
     } else {
-        imageElement.src = './img/like.png';
-        imageElement.alt = 'Like';
-        count -= 1;
-        counterElement.textContent = count > 0 ? count : '';
-        localStorage.removeItem(`liked${i}`);
+        post.liked = true;
+        post.likes += 1;
     }
+    imageElement.innerHTML = post.liked ? '❤️' : '🤍';
+    counterElement.textContent = post.likes > 0 ? post.likes : '';
+    saveArrayToLocalStorage('posts', posts);
 }
-
 
 
 function addComment(i) {
